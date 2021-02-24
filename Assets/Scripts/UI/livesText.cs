1f0d8f45a1c75937b0beeb29c129cb5e924ca666
normyp <pm.normington@gmail.com>
2021-02-24 11:41:05 +0000

    Make lives work

--- Assets/Scripts/UI/livesText.cs
+++ Assets/Scripts/UI/livesText.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class livestext : MonoBehaviour {
+public class livesText : MonoBehaviour {
 
 	Text text;
 	public Texture tex;
